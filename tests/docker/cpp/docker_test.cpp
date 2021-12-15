bd5fc22dcdccb54c56e49c0986c44252fc50dc9c
Matthew Ellis <matthew.ellis@hpe.com>
2021-12-15 13:26:32 -0800

    Improve C++ docker test.

---
+++ tests/docker/cpp/docker_test.cpp
@@ -0,0 +1,13 @@
+#include "client.h"
+
+int main(int argc, char* argv[]) {
+
+    SmartRedis::Client client(false);
+
+    std::vector<double> data = {1.0, 2.0, 3.0};
+    std::vector<size_t> dims = {3};
+    client.put_tensor("cpp_docker_tensor", data.data(), dims,
+                      SmartRedis::TensorType::dbl,
+                      SmartRedis::MemoryLayout::contiguous);
+    return 0;
+}
