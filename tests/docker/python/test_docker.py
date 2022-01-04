7f3246fc7a03781c7544788371a2f6d0295112be
Matthew Ellis <matthew.ellis@hpe.com>
2022-01-04 15:48:19 -0800

    Update and run python docker test.

--- tests/docker/python/test_docker.py
+++ tests/docker/python/test_docker.py
@@ -1,2 +1,8 @@
 from smartredis import Client
-from time import sleep
+import numpy as np
+
+client = Client(None, False)
+
+tensor = np.random.randint(-10, 10, size=(2,4))
+
+client.put_tensor("python_docker_tensor", tensor)
